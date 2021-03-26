c3fc14453a41d3b6fe4ab0bfd4deb2e1abfc7c4f
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-03-26 20:17:17 +0100

    setup scene, camera, render function and add basic cube

--- src/script.js
+++ src/script.js
@@ -1,4 +1,34 @@
 import './style.css';
 import * as THREE from 'three';
 
-console.log(THREE);
+const scene = new THREE.Scene();
+const canvas = document.querySelector('.webgl');
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+cube.rotation.y = 10;
+cube.rotation.x = 10;
+
+const size = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    size.width / size.height,
+    0.1,
+    1000,
+);
+camera.position.z = 5;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
